a0ec9f3764c03e4f13d796c30fca0e24cd4c5e62
Wannaphong <wannaphong@yahoo.com>
2023-03-24 09:15:35 +0000

    dump wiki_20230101

--- clean.py
+++ clean.py
@@ -1,8 +1,8 @@
 def clean_text(text):
     return text.strip().replace('()','').replace('( )','').replace('(; ;)','').replace('(; ; )','').replace('(; )','').replace('(, )','')
 
-with open('wiki_20210620.txt', 'r', encoding='utf-8-sig') as f:
+with open('wiki_20230101.txt', 'r', encoding='utf-8-sig') as f:
     data = clean_text(f.read())
 
-with open('wiki_20210620_clean.txt', 'w', encoding='utf-8') as f:
+with open('wiki_20230101_clean.txt', 'w', encoding='utf-8') as f:
     f.write(data)
